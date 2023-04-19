d192670df70ec5eb71744bab43ab1e4c27ed4825
memleaker <memleak@126.com>
2023-04-19 23:36:09 +0800

    添加SSL相关代码

--- src/amani.cpp
+++ src/amani.cpp
@@ -1,12 +1,18 @@
 
+#include <signal.h>
+
 #include "utils.h"
 #include "stats.h"
+#include "ssl_client.h"
 #include "thpool/thpool.h"
 #include "copool/copool.h"
 
 int main(int argc, char **argv)
 {
     stats st;
+    ssl_client sslc(1, ssl_client::openssl::init_ssl_env());
+
+    signal(SIGPIPE, SIG_IGN);
     
     thread_pool thpool{static_cast<int>(utils::cpu_num(4))};
 
